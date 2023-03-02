b1fe5a714b0a1c60044467d436c4ad9f03ca7d41
RichardPMachado <richardmachado2211@gmail.com>
2023-03-02 03:45:20 -0300

    feat[fluxo 2]: criando login controller

---
+++ app/backend/src/api/controller/loginController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import InvalidEmailOrPasswordError from '../helpers/InvalidEmailOrPasswordError';
+import { IUserRepository } from '../interfaces/UserInterfaces';
+
+export default class LoginController {
+  constructor(
+    private _service: IUserRepository,
+  ) {}
+
+  async authLogin(request: Request, response: Response) {
+    const { email, password } = request.body;
+    const token = await this._service.getByLogin(email, password);
+
+    if (!token) {
+      throw new InvalidEmailOrPasswordError('Invalid email or password');
+    }
+    return response.status(200).json({ token });
+  }
+}

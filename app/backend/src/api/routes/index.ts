0e5c1226294d56df87cf55418e840d7306af46b5
RichardPMachado <richardmachado2211@gmail.com>
2023-03-03 17:01:55 -0300

    feat[fluxo 3 - requisito 13]: criando routes

--- app/backend/src/api/routes/index.ts
+++ app/backend/src/api/routes/index.ts
@@ -1,4 +1,5 @@
 import teamsRouter from './teamsRouter';
 import loginRouter from './loginRouter';
+import matchesRouter from './matchesRouter';
 
-export { teamsRouter, loginRouter };
+export { teamsRouter, loginRouter, matchesRouter };
---
+++ app/backend/src/api/routes/matchesRouter.ts
@@ -0,0 +1,12 @@
+import { Router, Response, Request } from 'express';
+import { MatchesController } from '../controller';
+import { MatchesService } from '../services';
+
+const router = Router();
+const machesService = new MatchesService();
+const matchesController = new MatchesController(machesService);
+
+router.get('/matches', (request: Request, response: Response) => matchesController
+  .getAll(request, response));
+
+export default router;

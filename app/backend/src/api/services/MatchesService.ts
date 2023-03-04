c27d40e240db47dca3a8281a351aff6def4cd9fc
RichardPMachado <richardmachado2211@gmail.com>
2023-03-03 22:52:47 -0300

    feat[fluxo 3- requisito 16]: configurando a matches service de query

--- app/backend/src/api/services/MatchesService.ts
+++ app/backend/src/api/services/MatchesService.ts
@@ -14,7 +14,17 @@ export default class MatchesService implements IMatchesRepository {
       ],
     });
 
-    console.log(matches);
+    return matches;
+  }
+
+  async getByQuery(): Promise<IMatches[]> {
+    const matches = await this.model.findAll({
+      where: { inProgress: true },
+      include: [
+        { model: Team, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+    });
     return matches;
   }
 }

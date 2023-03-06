0df26a86adc786e2006233e70bb146eb8c4b3f30
RichardPMachado <richardmachado2211@gmail.com>
2023-03-06 15:09:49 -0300

    rafactor[fluxo 4 - requisito 23]: incluindo a rota leaderboard no app.ts

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,6 +1,6 @@
 import 'express-async-errors';
 import * as express from 'express';
-import { teamsRouter, loginRouter, matchesRouter } from './api/routes';
+import { teamsRouter, loginRouter, matchesRouter, leaderboardRouter } from './api/routes';
 import errorMiddleware from './api/middleware/error';
 
 class App {
@@ -16,7 +16,7 @@ class App {
     this.app.use(teamsRouter);
     this.app.use(loginRouter);
     this.app.use(matchesRouter);
-
+    this.app.use(leaderboardRouter);
     this.app.use(errorMiddleware);
   }
 
